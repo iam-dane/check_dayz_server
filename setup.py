b4d2db3a2694b48b31ceefd724f16d60c0c4985a
dane <danez0r@gmail.com>
2022-07-30 15:49:14 -0700

    change to basic setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from setuptools import setup
+import setuptools
 
 
-setup(setup_requires=['pbr'], pbr=True)
+setuptools.setup(
+    name='check_dayz_server',
+    version='0.1.0',
+    description='Check DayZ server player count.',
+    packages=['check_dayz_server'],
+    install_requires=[
+        'requests==2.28.1',
+        'python-dotenv==0.20.0'
+    ],
+    entry_points={
+        'console_scripts': [(
+            'check_dayz_server='
+            'check_dayz_server.__main__:main'
+        )]
+    }
+)
